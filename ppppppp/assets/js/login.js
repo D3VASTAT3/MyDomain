515fb9968d3e49311b70570064000f38f7f73660
toni225 <73512652+toni225@users.noreply.github.com>
2023-02-15 22:24:40 +0800

    Update login.js

--- ppppppp/assets/js/login.js
+++ ppppppp/assets/js/login.js
@@ -22,7 +22,10 @@ login.addEventListener('click', async e => {
 
         console.log(data,error);
         
-    
+        if(data.session.user.email === email){
+            alert('welcome!');
+            window.location.replace("https://youtu.be/dQw4w9WgXcQ");
+        }
         
     }
     
